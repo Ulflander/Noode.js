74f3067097147de726bc87640334c6cc76c3e60a
Xavier Laumonier <xlaumonier@gmail.com>
2011-06-08 16:41:44 +0200

    Minor fixes

--- example.js
+++ example.js
@@ -1,13 +1,65 @@
-var noode = require('noode');
+
+// We require the noode module
+var noode = require(__dirname+'/lib/noode.js');
 
 
+
+
+// A very basic class
+var Basic = noode.Class.extend({
+	
+	// That say hello
+	sayHello: function () {
+		console.log('Hello') ;
+	}
+	
+});
+
+var basic = new Basic () ;
+basic.sayHello () ; // => 'Hello'
+
+
+
+
+
+// Another basic class with inheritance
+var BasicChild = Basic.extend({
+
+	// That say hello too
+	sayHello: function () {
+		console.log('Hello world') ;
+		
+		// We call super method
+		this._super () ;
+	}
+	
+});
+
+var basicChild = new BasicChild () ;
+basicChild.sayHello () ; // => 'Hello world', 'Hello'
+
+
+
+// An exemple of some class extending AbstractEventDispatcher to dispatch events
 var Foo = noode.AbstractEventDispatcher.extend({
 	construct: function( content ){
+
 		this.content = content;
+		
 	},
+	// Here we will dispatch an event
 	getContent: function(){
-	
+		// An easy way to dispatch events without creating an Event object, AbstractEventDispatcher will do it for you
 		if ( this.dispatch('getContent', {content: this.content} ) )
+		{
+			return this.content ;
+		}
+	},
+	
+	getOddContent: function ()
+	{
+		// The common way to dispatch event
+		if ( this.dispatch( new noode.Event ( 'getContent', {content: this.content} ) ) )
 		{
 			return this.content ;
 		}
@@ -15,34 +67,97 @@ var Foo = noode.AbstractEventDispatcher.extend({
 });
 
 
+var foo = new Foo( 'test' );
+console.log ( foo.getContent() ) ; // => 'test'
+console.log ( foo.getOddContent() ) ; // => 'test'
 
 
+
+// Inheritance again
 var Bar = Foo.extend({
 	construct: function( content ){
+		// Call the super constructor
 		this._super('Bar-' + content ) ;
 	}
 });
 
 
 
-var foo = new Foo( 'test' );
-console.log ( foo.getContent() ) ; // => 'test'
-
 var bar = new Bar( 'test' );
 console.log ( bar.getContent() ) ; // => 'Bar-test'
 
+
+
+// Lets add some listeners
+
+// Here is a callback used for our listeners
 var callback = function ( event )
 {
+	// If event parameters dont pleased us
 	if ( event.content == 'test' )
 	{
-		return true ;
+		// We prevent the default action
+		event.prevent () ;
 	}
-	return false ;
 };
 
-bar.addListener( 'getContent' , callback);
-foo.addListener( 'getContent' , callback );
+// We attach our listeners
+bar.on( 'getContent' , callback);
+foo.on( 'getContent' , callback );
 
-
+// Lets try our listeners
+console.log ( bar.getContent() ) ; // => 'Bar-test'
 console.log ( foo.getContent() ) ; // => undefined
-console.log ( bar.getContent() ) ; // => 'Bar-test
+
+
+
+
+
+
+//Lets create our own event
+var OddEvent = noode.Event.extend({
+	
+	construct: function ( parameters )
+	{
+		this._super ( 'oddEvent' , parameters ) ;
+	},
+	
+	getOddEventThing: function ()
+	{
+		return 'That\'s odd' ;
+	}
+	
+});
+
+// The class that will dispatch the OddEvent
+var OddClass = noode.AbstractEventDispatcher.extend ({
+	
+	dispatchOddEvent: function ()
+	{
+		this.dispatch ( new OddEvent() ) ;
+	}
+	
+});
+
+
+var odd = new OddClass () ;
+
+// We listen to our event
+odd.on('oddEvent', function ( event )
+{
+	// Test event
+	console.log ( event.getOddEventThing () ) ;
+});
+
+// and dispatch it
+odd.dispatchOddEvent () ; // => 'That's odd'
+
+
+
+
+
+
+
+
+
+
